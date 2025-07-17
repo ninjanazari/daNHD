4ebe45d3ac762b2cb04b105edcb780554cb5cc20
nazar <210274@studierende.htl-donaustadt.at>
2025-07-17 19:15:01 +0200

    Navigation zwischen den Seiten erstellt

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from '../pages/Login/login';
+import {RegisterComponent} from '../pages/Register/register';
+import {EmployeesComponent} from '../pages/Employees/employees';
+import {ProjectsComponent} from '../pages/Projects/projects';
+import {ScheduleComponent} from '../pages/Schedule/schedule';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'employees',
+    component: EmployeesComponent
+  },
+  {
+    path: 'projects',
+    component: ProjectsComponent
+  },
+  {
+    path: 'schedule',
+    component: ScheduleComponent
+  }
+];
